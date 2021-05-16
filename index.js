b139071bde9700dac9f6ccaec5b525dd0841ef5a
Meriç Gerçeker <meric.gerceker@lunatr.com>
2021-05-16 18:05:29 +0300

    Düzeltme

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ document.getElementById("addToDoButton").addEventListener('click', addToDo);
 function addToDo() {
     var newActivity = document.getElementById("toDoInput").value;
 
+    if (newActivity == "") {
+        errorMessage("Listeye boş ekleme yapılamaz.");
+        return;
+    }
+
     var trElement = document.createElement("tr");
     trElement.classList.add("align-middle");
 
     var tdElement = document.createElement("td");
+    tdElement.classList.add("to-do-text");
     tdElement.innerText = newActivity;
 
     var tdCloseElement = document.createElement("td");
@@ -21,7 +27,6 @@ function addToDo() {
     buttonElement.addEventListener('click', function() {
         deleteToDo(this);
     }, false);
-    
 
     tdCloseElement.appendChild(buttonElement);
 
@@ -30,9 +35,58 @@ function addToDo() {
 
     document.getElementById("toDoTableBody").appendChild(trElement);
 
+    trElement.addEventListener('click', function() {
+        checkToDo(this);
+    }, false);
+
     document.getElementById("toDoInput").value = "";
+
+    successMessage("Ekleme işlemi başarıyla gerçekleştirildi.");
 }
 
 function deleteToDo(element) {
     element.closest("tr").remove();
+}
+
+function checkToDo(element) {
+    debugger
+    var toDoTextElement = element.querySelector("td.to-do-text");
+    var toDoText = "";
+
+    if (element.classList.contains("checked")) {
+        toDoText = toDoTextElement.querySelector("del").innerText;
+        toDoTextElement.innerText = toDoText;
+    } else {
+        toDoText = toDoTextElement.innerText;
+
+        var delElement = document.createElement("del");
+        delElement.innerText = toDoText;
+
+        toDoTextElement.innerText = "✓ ";
+
+        toDoTextElement.appendChild(delElement);
+    }
+
+    element.classList.toggle("checked");
+
+}
+
+function successMessage(message) {
+    swal({
+        title: "Başarılı!",
+        text: message,
+        type: "success",
+        timer: 1500,
+        showConfirmButton: false
+      });
+}
+
+function errorMessage(message) {
+    swal({
+        title: "Hata!",
+        text: message,
+        type: "error",
+        timer: 1500,
+        showConfirmButton: false
+      });
 }
